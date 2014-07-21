51d0ab1f38031a588eb72d2668c837c60864ac36
harishankarsomasundaram <harishankar.info@gmail.com>
2014-07-22 01:43:52 +0530

    init

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HighChartDemo.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HighChartDemo.Startup))]
+namespace HighChartDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
